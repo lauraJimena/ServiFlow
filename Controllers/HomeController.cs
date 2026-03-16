f0ef256718639cce0972145e7a563fbb9bece929
MateoM <andresmateo566@gamil.com>
2026-03-16 07:47:12 -0500

    Paginación, arreglo vista principal y tabla de agregar y eliminar

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,9 +11,24 @@ public class HomeController : Controller
         _context = context;
     }
 
-    public IActionResult Index()
+    public IActionResult Index(int page = 1)
     {
-        var emprendimientos = _context.Emprendimientos.ToList();
+        int pageSize = 10;
+
+        var query = _context.Emprendimientos
+            .AsNoTracking()
+            .OrderBy(e => e.Id);
+
+        int totalItems = query.Count();
+
+        var emprendimientos = query
+            .Skip((page - 1) * pageSize)
+            .Take(pageSize)
+            .ToList();
+
+        ViewBag.CurrentPage = page;
+        ViewBag.TotalPages = (int)Math.Ceiling(totalItems / (double)pageSize);
+
         return View(emprendimientos);
     }
 }
